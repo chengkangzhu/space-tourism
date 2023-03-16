2820ed7a4b30f0d84b2cab9ce97338685108e9c3
Cheng Kang <y2984491@gmail.com>
2023-03-16 17:19:03 -0400

    change js to jquery

--- index.js
+++ index.js
@@ -1,61 +1,148 @@
+$(".nav-text").click(function () {
+	// Remove active class from all nav links
+	$(".nav-text").removeClass("active");
+	// Add active class to clicked nav link
+	$(this).addClass("active");
+});
 
+// function change_destination(destination) {
+// 	if (destination != undefined) {
+// 		//change active state
+// 		document
+// 			.querySelectorAll(".destination-content-navbar li a")
+// 			.forEach((i) => i.classList.remove("active"));
+// 		document.getElementById(destination).classList.add("active");
 
+// 		//change content
+// 		for (let i in data["destinations"]) {
+// 			if (destination == data["destinations"][i]["name"]) {
+// 				document.querySelector(".destination-name").innerHTML =
+// 					data["destinations"][i]["name"];
+// 				document.querySelector(".destination-info").innerHTML =
+// 					data["destinations"][i]["description"];
+// 				document.querySelector(".destination-distance").innerHTML =
+// 					data["destinations"][i]["distance"];
+// 				document.querySelector(".destination-time").innerHTML =
+// 					data["destinations"][i]["travel"];
+// 				document
+// 					.querySelector(".destination-image")
+// 					.setAttribute(
+// 						"src",
+// 						data["destinations"][i]["images"]["png"]
+// 					);
+// 			}
+// 			//change active state
+// 			document
+// 				.querySelectorAll(".small-dots")
+// 				.forEach((i) => i.classList.remove("active"));
+// 			document.getElementById(destination).classList.add("active");
+// 		}
+// 	}
+// }
 
+// function change_crew(role) {
+// 	if (role != undefined) {
+// 		//loop data , change content
+// 		for (let i in data["crew"]) {
+// 			if (role == data["crew"][i]["role"]) {
+// 				setTimeout(() => {
+// 					document.querySelector(".crew-role").innerHTML =
+// 						data["crew"][i]["role"];
+// 					document.querySelector(".crew-name").innerHTML =
+// 						data["crew"][i]["name"];
+// 					document.querySelector(".crew-info").innerHTML =
+// 						data["crew"][i]["bio"];
+// 					document
+// 						.querySelector(".crew-image")
+// 						.setAttribute("src", data["crew"][i]["images"]["webp"]);
+// 				}, 100);
+// 			}
+// 			//change active state
+// 			document
+// 				.querySelectorAll(".small-dots")
+// 				.forEach((i) => i.classList.remove("active"));
+// 			document.getElementById(role).classList.add("active");
+// 		}
+// 	}
+// }
+// function change_technology(technology) {
+// 	if (technology != undefined) {
+// 		//loop data , change content
+// 		for (let i in data["technology"]) {
+// 			if (technology == data["technology"][i]["name"]) {
+// 				setTimeout(() => {
+// 					document.querySelector(".technology-name").innerHTML =
+// 						data["technology"][i]["name"];
+// 					document.querySelector(".technology-info").innerHTML =
+// 						data["technology"][i]["description"];
 
+// 					document
+// 						.querySelector(".technology-image-portrait")
+// 						.setAttribute(
+// 							"src",
+// 							data["technology"][i]["images"]["portrait"]
+// 						);
+// 					document
+// 						.querySelector(".technology-image-landscape")
+// 						.setAttribute(
+// 							"src",
+// 							data["technology"][i]["images"]["landscape"]
+// 						);
+// 				}, 100);
+// 			}
+// 			//change active state
+// 			document
+// 				.querySelectorAll(".technology-navtext")
+// 				.forEach((i) => i.classList.remove("active"));
+// 			document.getElementById(technology).classList.add("active");
+// 		}
+// 	}
+// }
+
+//responsive functions
+//navbar menu
+// let navbar = document.querySelector("#navbar");
+// document.querySelectorAll(".navbar-icon-js").forEach((link) => {
+// 	link.addEventListener("click", function () {
+// 		navbar.classList.toggle("hide-navbar");
+// 	});
+// });
+// document.querySelectorAll("#navbar li a").forEach((link) => {
+// 	link.addEventListener("click", function () {
+// 		navbar.classList.toggle("hide-navbar");
+// 	});
+// });
 
 function change_destination(destination) {
-	if (destination != undefined) {
-		//change active state
-		document
-			.querySelectorAll(".destination-content-navbar li a")
-			.forEach((i) => i.classList.remove("active"));
-		document.getElementById(destination).classList.add("active");
+	if (destination !== undefined) {
+		// change active state
+		$(".destination-content-navbar li a").removeClass("active");
+		$("#" + destination).addClass("active");
 
-		//change content
-		for (let i in data["destinations"]) {
-			if (destination == data["destinations"][i]["name"]) {
-				document.querySelector(".destination-name").innerHTML =
-					data["destinations"][i]["name"];
-				document.querySelector(".destination-info").innerHTML =
-					data["destinations"][i]["description"];
-				document.querySelector(".destination-distance").innerHTML =
-					data["destinations"][i]["distance"];
-				document.querySelector(".destination-time").innerHTML =
-					data["destinations"][i]["travel"];
-				document
-					.querySelector(".destination-image")
-					.setAttribute(
-						"src",
-						data["destinations"][i]["images"]["png"]
-					);
-			}
-			//change active state
-			document
-				.querySelectorAll(".small-dots")
-				.forEach((i) => i.classList.remove("active"));
-			document.getElementById(destination).classList.add("active");
-		}
+		// change content
+		var dest = data.destinations.find((d) => d.name === destination);
+		$(".destination-name").html(dest.name);
+		$(".destination-info").html(dest.description);
+		$(".destination-distance").html(dest.distance);
+		$(".destination-time").html(dest.travel);
+		$(".destination-image").attr("src", dest.images.png);
+
+		// change active state
+		$(".small-dots").removeClass("active");
+		$("#" + destination).addClass("active");
 	}
 }
-
 function change_crew(role) {
 	if (role != undefined) {
-		//loop data , change content
-		for (let i in data["crew"]) {
-			if (role == data["crew"][i]["role"]) {
-				setTimeout(() => {
-					document.querySelector(".crew-role").innerHTML =
-						data["crew"][i]["role"];
-					document.querySelector(".crew-name").innerHTML =
-						data["crew"][i]["name"];
-					document.querySelector(".crew-info").innerHTML =
-						data["crew"][i]["bio"];
-					document
-						.querySelector(".crew-image")
-						.setAttribute("src", data["crew"][i]["images"]["webp"]);
-				}, 100);
-			}
-			//change active state
+		let crew = data["crew"].find((c) => c.role === role);
+		if (crew) {
+			setTimeout(() => {
+				$(".crew-role").html(crew.role);
+				$(".crew-name").html(crew.name);
+				$(".crew-info").html(crew.bio);
+				$(".crew-image").attr("src", crew.images.webp);
+			}, 100);
+
 			document
 				.querySelectorAll(".small-dots")
 				.forEach((i) => i.classList.remove("active"));
@@ -66,29 +153,21 @@ function change_crew(role) {
 
 function change_technology(technology) {
 	if (technology != undefined) {
-		//loop data , change content
-		for (let i in data["technology"]) {
-			if (technology == data["technology"][i]["name"]) {
-				setTimeout(() => {
-					document.querySelector(".technology-name").innerHTML =
-						data["technology"][i]["name"];
-					document.querySelector(".technology-info").innerHTML =
-						data["technology"][i]["description"];
-						
-					document
-					.querySelector(".technology-image-portrait")
-					.setAttribute(
-						"src",
-						data["technology"][i]["images"]["portrait"]
-					);
-					document
-						.querySelector(".technology-image-landscape")
-						.setAttribute(
-							"src",
-							data["technology"][i]["images"]["landscape"]
-						);
-				}, 100);
-			}
+		let tech = data["technology"].find((t) => t.name === technology);
+		if (tech) {
+			setTimeout(() => {
+				$(".technology-name").html(tech.name);
+				$(".technology-info").html(tech.description);
+				$(".technology-image-portrait").attr(
+					"src",
+					tech.images.portrait
+				);
+				$(".technology-image-landscape").attr(
+					"src",
+					tech.images.landscape
+				);
+			}, 100);
+
 			//change active state
 			document
 				.querySelectorAll(".technology-navtext")
@@ -104,23 +183,13 @@ fetch("./data.json")
 	})
 	.then(function (d) {
 		data = d;
-		console.log(d);
 	});
 
-//responsive functions
-//navbar menu
-let navbar = document.querySelector("#navbar");
-document.querySelectorAll(".navbar-icon-js").forEach((link) => {
-	link.addEventListener("click", function () {
-		navbar.classList.toggle("hide-navbar");
-	});
-});
-document.querySelectorAll("#navbar li a").forEach((link) => {
-	link.addEventListener("click", function () {
-		navbar.classList.toggle("hide-navbar");
-	});
+let navbar = $("#navbar");
+$(".navbar-icon-js").click(function () {
+	navbar.toggleClass("hide-navbar");
 });
 
-$(document).ready(function () {
-	console.log("jQuery is working!");
+$("#navbar li a").click(function () {
+	navbar.toggleClass("hide-navbar");
 });
